2f032702708852f30d666d4cee9d9cdbdca94daa
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-03-05 13:25:21 -0300

    feat: component signIn and SignUp

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
+
+import {NavigationContainer} from "react-navigation/native"
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  )
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import {View, ActivityIndicator} from "react-native";
+
+import AuthRoutes from "./auth.routes";
+
+function Routes(){
+  const loading = false;
+  const signed = false;
+
+  return(
+    signed ? <View></View> : <AuthRoutes/>
+  )
+}
+
+export default Routes;
