a732020e7484bb8b4b2ed0fb13ff374fb4599497
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-03-06 19:15:41 -0300

    feat: initialized styles SignIn

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {SafeAreaView, Text,} from "react-native"
+
+import { Background } from "./styles";
+
+
+export default function SignIn(){
+  return(
+    
+    <Background>
+      <Text>TELA LOGIN</Text> 
+    </Background>
+    
+  )
+}
