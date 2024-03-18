e3a0d07f94437d675c792fd7abd34b6119aacaeb
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-03-18 12:56:08 -0300

    feat: component SignUp

--- App.jsx
+++ App.jsx
@@ -3,13 +3,16 @@ import { StatusBar, SafeAreaView } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native"
 
 import Routes from './src/routes';
+import AuthProvider from './src/contexts/auth';
 
 export default function App() {
   return(
     <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
+      <AuthProvider>
       <StatusBar backgroundColor='#F0F4FF' barStyle="dark-content"/>
       <Routes/>
+      </AuthProvider>
     </NavigationContainer>
     </SafeAreaView>
   )
