b76c5980e2a455105d6e31f8bb70515c390a98be
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-04-04 12:21:03 -0300

    feat: Header Finished

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,13 +3,14 @@ import { View, Text, Button } from 'react-native';
 
 import { AuthContext } from "../../contexts/auth";
 
+import Header from "../../components/Header";
 import { Background } from "./styles";
 
 export default function Home(){
 
   return(
     <Background>
-
+      <Header title="Minhas movimentações"/>
     </Background>
   )
 }
