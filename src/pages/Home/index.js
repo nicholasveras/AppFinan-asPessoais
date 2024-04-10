06b11aaa89e39c8b87e129e6286910479750ecdf
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-04-10 13:03:25 -0300

    create formated date

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { View, Text, Button } from 'react-native';
 
 import { AuthContext } from "../../contexts/auth";
@@ -6,7 +6,36 @@ import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
 import { Background } from "./styles";
 
+import api from "../../services/api";
+import {format} from 'date-fns';
+
 export default function Home(){
+  const [listBalance, setListBalance] = useState([]);
+
+  const [dateMovements, setDateMovements] = useState(new Date())
+
+  useEffect(()=>{
+    let isActive = true;
+
+    async function getMovements(){
+      let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+
+      const balance = await api.get('/balance', {
+        params:{
+          date: dateFormated
+        }
+      })
+      console.log(balance.data);
+
+      if(isActive){
+        setListBalance(balance.data)
+      }
+    }
+
+    getMovements();
+
+    return () => isActive = false;
+  }, [])
 
   return(
     <Background>
